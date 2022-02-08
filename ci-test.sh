70047e0a2a65bf1ad47625600af067ba1f9b1e55
Yaron Sheffer <yaronf@gmx.com>
2022-02-09 00:49:06 +0200

    CI tests

--- ci-test.sh
+++ ci-test.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 # create config database
-go test ./rc &>  /dev/null
+go test ./rc &>  /dev/null || echo "DB created"
 # Ignore errors
 
 go test ./as &
 if [ $? ]; then
+	echo AS failed
 	exit 1
 fi
 
 go test ./rc || exit 1
+if [ $? ]; then
+	echo RC failed
+	exit 1
+fi
